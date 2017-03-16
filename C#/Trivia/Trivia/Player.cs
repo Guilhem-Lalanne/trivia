d3a0091d5a4254ca445692e9b15d33a7bfda27dd
LALANNE GUILHEM p0606160 <p0606160@ucbl.fr>
2017-03-16 17:29:20 +0100

    Completion of Player class : adding Place, Purse, IsInPenaltyBox. Not used in other classes.

--- C#/Trivia/Trivia/Player.cs
+++ C#/Trivia/Trivia/Player.cs
@@ -15,12 +15,19 @@ namespace Trivia
              */
             this.Id = Guid.NewGuid();
             this.Name = name;
+            this.Place = 0;
+            this.Purse = 0;
+            this.IsInPenaltyBox = false;
         }
 
         public Guid Id { get; private set; }
 
         public string Name { get; private set; }
 
+        public int Place { get; private set; }
 
+        public int Purse { get; private set; }
+
+        public bool IsInPenaltyBox { get; private set; }
     }
 }
