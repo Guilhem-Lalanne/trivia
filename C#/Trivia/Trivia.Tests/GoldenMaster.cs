f1b3ede3f2bedaaeb8a59b6fa536b3efdbbe9a23
LALANNE GUILHEM p0606160 <p0606160@ucbl.fr>
2017-03-30 16:47:06 +0200

    TP du 16/3 : ApprovalTests

---
+++ C#/Trivia/Trivia.Tests/GoldenMaster.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace Trivia.Tests
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class GoldenMaster
+    {
+        [Test]
+        public void ShouldNotChange()
+        {
+            // var est la déclartion du type qui suit
+            var stringWriter = new StringWriter();
+
+            // Svg du contenu de la console :
+            var previousConsoleOut = Console.Out;
+            Console.SetOut(stringWriter);
+            GameRunner.Main(null);
+
+            // reset de la sortie console
+            Console.SetOut(previousConsoleOut);
+            Approvals.Verify(stringWriter.ToString());
+        }
+    }
+}
