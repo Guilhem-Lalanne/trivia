d69e6e18b2d8f6640cd34864958ec1fe4491930e
p0606160 <guilhem.lalanne@etu.univ-lyon1.fr>
2017-04-13 18:23:52 +0200

    Deleted specific names for question lists, now standard. They have been included in a new List. Done with Guillaume Deroguerre.

--- C#/Trivia/Trivia/Questions.cs
+++ C#/Trivia/Trivia/Questions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,47 +12,57 @@ namespace Trivia
     {
         private readonly Categories _categories = new Categories();
 
-        LinkedList<string> popQuestions = new LinkedList<string>();
-        LinkedList<string> scienceQuestions = new LinkedList<string>();
-        LinkedList<string> sportsQuestions = new LinkedList<string>();
-        LinkedList<string> rockQuestions = new LinkedList<string>();
+        // Tableau de listes de questions par catégorie
+        public List<LinkedList<string>> Tab = new List<LinkedList<string>>();
 
         public Questions()
         {
-            for (var i = 0; i < 50; i++)
+            // Décompte des catégories pour bouclage :
+            var end = _categories.categories.Count();
+
+            // Pour chaque catégorie, création d'une liste (tas de cartes), inclue dans le tableau
+            for (var j = 0; j < end; j++)
             {
-                popQuestions.AddLast("Pop Question " + i);
-                scienceQuestions.AddLast("Science Question " + i);
-                sportsQuestions.AddLast("Sports Question " + i);
-                rockQuestions.AddLast("Rock Question " + i);
+                // Déclaration d'une liste de question (selon catégorie)
+                Tab.Add(new LinkedList<string>());
+
+                // Remplissage des questions pour la catégorie en cours
+                // TODO : à finir
+                for (var i = 0; i < 50; i++)
+                {
+                    Tab[j].AddLast(_categories.categories[j] + " Question " + i);
+                }
             }
+            
         }
 
         // Question dépendant de la place
         public void AskQuestion(int currentPlayerPlace)
         {
             // Détermniation de la catgorie, en fonction de la place
+            // TODO : calquer numéro de place sur numéro de catégorie
             Console.WriteLine("The category is " + _categories.CurrentCategory(currentPlayerPlace));
 
+            // TODO : prendre l'entier de référence de la catégorie en paramètre
             if (_categories.CurrentCategory(currentPlayerPlace) == "Pop")
             {
-                Console.WriteLine(popQuestions.First());
-                popQuestions.RemoveFirst();
+                Console.WriteLine(Tab[0].First());
+                Tab[0].RemoveFirst();
             }
             if (_categories.CurrentCategory(currentPlayerPlace) == "Science")
             {
-                Console.WriteLine(scienceQuestions.First());
-                scienceQuestions.RemoveFirst();
+                Console.WriteLine(Tab[1].First());
+                Tab[1].RemoveFirst();
             }
             if (_categories.CurrentCategory(currentPlayerPlace) == "Sports")
             {
-                Console.WriteLine(sportsQuestions.First());
-                sportsQuestions.RemoveFirst();
+                Console.WriteLine(Tab[2].First());
+                Tab[2].RemoveFirst();
             }
             if (_categories.CurrentCategory(currentPlayerPlace) == "Rock")
             {
-                Console.WriteLine(rockQuestions.First());
-                rockQuestions.RemoveFirst();
+                Console.WriteLine(Tab[3].First());
+                Tab[3].RemoveFirst();
             }
         }
 
